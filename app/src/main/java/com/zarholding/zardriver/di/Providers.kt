394ee92a6c7f4d0b24003b4df21f2a57435f3e7c
zar <m3.latifi@gmail.com>
2023-10-08 19:09:54 +0330

    تنظیم دامنه جدید

--- app/src/main/java/com/zarholding/zardriver/di/Providers.kt
+++ app/src/main/java/com/zarholding/zardriver/di/Providers.kt
@@ -17,8 +17,9 @@ import javax.inject.Singleton
 class Providers {
 
     companion object {
-        const val url = "http://5.160.125.98:5081"
+        //        const val url = "http://5.160.125.98:5081"
 //        const val url = "http://192.168.50.153:9090"//supperApp
+        const val url = "https://app.zarholding.com:5081"//supperApp
     }
 
 
